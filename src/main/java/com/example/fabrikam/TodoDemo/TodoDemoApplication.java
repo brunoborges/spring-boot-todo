5bf2a6c3e0e6fe66075d38a2d37ec903d7eb98bb
Bruno Borges <bruno.borges@microsoft.com>
2018-06-05 19:04:40 -0700

    upgrade to spring boot 2

--- src/main/java/com/example/fabrikam/TodoDemo/TodoDemoApplication.java
+++ src/main/java/com/example/fabrikam/TodoDemo/TodoDemoApplication.java
@@ -1,27 +1,21 @@
 package com.example.fabrikam.TodoDemo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 
 public class TodoDemoApplication extends SpringBootServletInitializer {
 
-	private static final Logger log = LoggerFactory.getLogger(TodoDemoApplication.class);
-
-	@Override
+    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(TodoDemoApplication.class);
     }
 
-
-	public static void main(String[] args) {
-		SpringApplication.run(TodoDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoDemoApplication.class, args);
+    }
+    
 }
